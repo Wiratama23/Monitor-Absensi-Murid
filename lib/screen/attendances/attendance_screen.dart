28716722cbe08139440d922099de7098f135a176
zakelyk <candra.setyo.w@gmail.com>
2024-01-24 12:41:59 +0700

    fixed years dropdown

--- lib/screen/attendances/attendance_screen.dart
+++ lib/screen/attendances/attendance_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:absensi_sd/dummyData.dart';
 import 'package:absensi_sd/routes/routes_name.dart';
 import 'package:absensi_sd/screen/attendances/components/attendance.dart';
@@ -5,15 +7,14 @@ import 'package:absensi_sd/screen/attendances/constants.dart';
 import 'package:absensi_sd/screen/attendances/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'dart:math' as math;
 
 class AttendanceSiswa extends GetView<AttendanceController> {
-  const AttendanceSiswa({super.key});
   static const String _nama = Dummy.nama;
   static const String _kelas = Dummy.kelas;
   static const String _nis = Dummy.nis;
   static const String _images = Dummy.image;
   static const List<Map<String, String>> presensi = Dummy.presensi;
+  const AttendanceSiswa({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -44,42 +45,61 @@ class AttendanceSiswa extends GetView<AttendanceController> {
             ),
             const SizedBox(height: 40),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
               child: Center(
                 child: Obx(
-                    ()=> DropdownButton(
+                  () => Row(children: [
+                    DropdownButton(
                         value: controller.currentMonth,
                         items: controller.months.map((String month) {
-                                return DropdownMenuItem<String>(
-                                value: month,
-                                child: Text(month, style: const TextStyle(fontSize: 25)));
+                          return DropdownMenuItem<String>(
+                              value: month,
+                              child: Text(month,
+                                  style: const TextStyle(fontSize: 25)));
                         }).toList(),
                         icon: const Icon(Icons.calendar_month),
-                        onChanged: (String? newVal){
-                          controller.currentMonthIndex.value = controller.months.indexOf(newVal!);
-                        }
-                    ),
-                    //     DropdownDatePicker(
-                    //     inputDecoration: InputDecoration(
-                    //         enabledBorder: const OutlineInputBorder(
-                    //           borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                    //         ),
-                    //         border: OutlineInputBorder(
-                    //             borderRadius: BorderRadius.circular(10))), // optional
-                    //     isDropdownHideUnderline: true, // optional
-                    //     startYear: 2023, // optional
-                    //     endYear: 2025, // optional
-                    //     width: 10, // optional
-                    //     selectedMonth: controller.month!.value, // optional
-                    //     selectedYear: controller.year!.value, // optional
-                    //     onChangedMonth: (value) => {
-                    //
-                    //       controller.onChangedMonth(int.parse(value!))
-                    //     },
-                    //     onChangedYear: (value) => controller.onChangedYear(int.parse(value!)),
-                    //     showDay: false,
-                    //     locale: "en"
-                    // ),
+                        onChanged: (String? newVal) {
+                          controller.currentMonthIndex.value =
+                              controller.months.indexOf(newVal!);
+                        }),
+                    const SizedBox(width: 10),
+                    DropdownButton(
+                        value: controller.currentYear.toString(),
+                        items: controller.years.map((int years) {
+                          return DropdownMenuItem<String>(
+                              value: years.toString(),
+                              child: Text(years.toString(),
+                                  style: const TextStyle(fontSize: 25)));
+                        }).toList(),
+                        onChanged: (String? newVal) {
+                          if (newVal != null) {
+                            controller.currentYear.value = int.parse(newVal);
+                          }
+                        }),
+                  ]),
+
+                  //     DropdownDatePicker(
+                  //     inputDecoration: InputDecoration(
+                  //         enabledBorder: const OutlineInputBorder(
+                  //           borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                  //         ),
+                  //         border: OutlineInputBorder(
+                  //             borderRadius: BorderRadius.circular(10))), // optional
+                  //     isDropdownHideUnderline: true, // optional
+                  //     startYear: 2023, // optional
+                  //     endYear: 2025, // optional
+                  //     width: 10, // optional
+                  //     selectedMonth: controller.month!.value, // optional
+                  //     selectedYear: controller.year!.value, // optional
+                  //     onChangedMonth: (value) => {
+                  //
+                  //       controller.onChangedMonth(int.parse(value!))
+                  //     },
+                  //     onChangedYear: (value) => controller.onChangedYear(int.parse(value!)),
+                  //     showDay: false,
+                  //     locale: "en"
+                  // ),
                 ),
               ),
             ),
--- lib/screen/attendances/controller.dart
+++ lib/screen/attendances/controller.dart
@@ -1,27 +1,81 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AttendanceController extends GetxController {
   final RxInt currentMonthIndex = 5.obs;
+  final RxInt currentYear = 2030.obs;
+  List<int> years = [];
 
   // List of months
   List<String> months = [
-    "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
   ];
-  
+
   String get currentMonth => months[currentMonthIndex.value];
+  // int get selectedYear => selectedYear;
+  // int get currentYear => years[currentYearIndex.value];
+
+  List<int> genYears(RxInt currentYear) {
+    List<int> years = [];
+    for (int i = 3; i >= 1; i--) {
+      years.add(currentYear.value - i);
+    }
+    years.add(currentYear.value);
+    for (int i = 1; i <= 3; i++) {
+      years.add(currentYear.value + i);
+    }
+    return years;
+  }
+
+  AttendanceController() {
+    years = genYears(currentYear);
+
+  }
 
   void incrementMonth() {
     if (currentMonthIndex.value < months.length - 1) {
       currentMonthIndex.value++;
+    } else if (currentMonthIndex.value > months.length - 1) {
+      currentYear.value++;
+      // currentYearIndex.value++;
+      currentMonthIndex.value = 0;
     }
   }
 
   void decrementMonth() {
     if (currentMonthIndex.value > 0) {
       currentMonthIndex.value--;
+    } else if (currentMonthIndex.value < 0) {
+      currentYear.value--;
+      // currentYearIndex.value--;
+      currentMonthIndex.value = months.length - 1;
     }
   }
+
+  // void incrementYear() {
+  //   if (currentYearIndex.value < years.length - 1) {
+  //     currentYearIndex.value++;
+  //   }
+  // }
+
+  // void decrementYear() {
+  //   if (currentYearIndex.value > 0) {
+  //     currentYearIndex.value--;
+  //   }
+  // }
 
   Widget buildLeadingIcon(String? status) {
     return Icon(
@@ -33,5 +87,4 @@ class AttendanceController extends GetxController {
   Color genColor(String? status) {
     return status == "Hadir" ? Colors.tealAccent : Colors.orange;
   }
-
 }
