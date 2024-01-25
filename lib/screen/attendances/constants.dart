850b013371988b27a7828e6ac84e5c82f067976c
zakelyk <candra.setyo.w@gmail.com>
2024-01-25 11:40:56 +0700

    Delete constants.dart

--- lib/screen/attendances/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Bulan {
-  static const List<String> months = [
-    "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"
-  ];
-}
